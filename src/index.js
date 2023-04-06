f60148ff467277de6f5417bce6b314ab00d0e8cc
axemanks <keithscheldt@gmail.com>
2023-04-06 09:45:06 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+
+import { App as Canvas } from "./Canvas";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(<Canvas />);
